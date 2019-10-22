df74a7ac5fd2874a8012feb0125ff7f43c609f77
Takuma Osada <judgment9ttk069@gmail.com>
2019-10-22 19:07:27 +0900

    Initial Commit

---
+++ TechBookFest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechBookFest
+//
+//  Created by 長田卓馬 on 2019/10/22.
+//  Copyright © 2019 Takuma Osada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
